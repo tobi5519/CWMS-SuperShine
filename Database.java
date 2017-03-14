b383f0c73c60495546a7c837b402a9fbbb9b1a4c
tobi5519 <tobi5519@stud.kea.dk>
2017-03-14 12:10:50 +0100

    ...

--- Database.java
+++ Database.java
@@ -3,11 +3,4 @@ import java.util.ArrayList;
 public class Database{
      ArrayList<Receipt> receiptlist = new ArrayList<Receipt>();
 
-     // public void printReceipt(int e){
-     //      System.out.print(Receipt.get(e));
-     // }
-
-     public String print(String info){
-          return info;
-     }
 }

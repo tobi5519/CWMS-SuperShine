a2f53c6d5241965e7373d6e59f02c383909a2304
Andreas <rednex702@hotmail.com>
2017-03-14 14:48:49 +0100

    addet constructor to ReceiptClass

--- Receipt.java
+++ Receipt.java
@@ -4,9 +4,18 @@ public class Receipt
 {
     private Date date;
     private Customer customer;
-    private CarWash washType;
+    private CarWash carwash;
     private double price;
     private int id;
+
+    public Receipt(Date date, Customer customer, CarWash carWash, double price, Int id)
+    {
+        this.date = date;
+        this.customer = customer;
+        this.carWash = carWash;
+        this.price = price;
+        this.id = id;
+    }
 
     @Override
     public String toString() 

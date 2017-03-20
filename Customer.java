4f6ba0e4e02a464fae29d2634c597c2709c0cf46
Andreas <rednex702@hotmail.com>
2017-03-20 11:29:11 +0100

    fjernet ID

--- Customer.java
+++ Customer.java
@@ -4,15 +4,15 @@ public class Customer
      private String lastName;
      private WashCard washCard;
      private CreditCard creditCard;
-     private int id;
+     
 
-     public Customer(String firstName, String lastName, WashCard washCard, CreditCard creditCard, int id) 
+     public Customer(String firstName, String lastName, WashCard washCard, CreditCard creditCard) 
      {
           this.firstName = firstName;
           this.lastName = lastName;
           this.washCard = washCard;
           this.creditCard = creditCard; 
-          this.id = id;
+          
      }
      
      public String getName()

357a3b4d643cf74115c5e8e526dcd7966a6c56f0
Andreas <rednex702@hotmail.com>
2017-03-20 11:42:01 +0100

    m

---
+++ Machine.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Machine  
+{
+    public static void main(String[] args) 
+    {        
+        ArrayList<Customer> customers = new ArrayList<Customer>(); 
+         
+    }
+}
